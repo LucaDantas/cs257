8dbda7981649b4b41cca1fa0b3f99911c72ff43e
Zimri Leisher <zimri.leisher@gmail.com>
2022-11-06 23:52:14 -0600

    Add problem id

--- webapp/doc/database-schema.sql
+++ webapp/doc/database-schema.sql
@@ -1,3 +1,3 @@
-CREATE TABLE users (handle text, first_name text, last_name text, rating integer, max_rating integer, user_rank text, max_user_rank text);
+CREATE TABLE users (handle text, first_name text, last_name text, country text, organization text, rating integer, max_rating integer, user_rank text, max_user_rank text);
 CREATE TABLE contests (id integer, total_solves integer);
-CREATE TABLE problems (contest_id integer, name text, idx text, rating integer, tags text, solved_count integer);
+CREATE TABLE problems (problem_id integer, contest_id integer, name text, idx text, rating integer, tags text, solved_count integer);
