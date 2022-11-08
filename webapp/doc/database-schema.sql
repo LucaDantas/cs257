9d9ebb17fb083ddaa96423d870be1dbfd713978f
LucaDantas <luca100sdantas2@gmail.com>
2022-11-07 23:40:57 -0600

    Update the problem id from an integer to a text
    The id is a combination of the contest index and the letter in the contest, therefore text not int

--- webapp/doc/database-schema.sql
+++ webapp/doc/database-schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (handle text, first_name text, last_name text, country text, organization text, rating integer, max_rating integer, user_rank text, max_user_rank text);
 CREATE TABLE contests (id integer, total_solves integer);
-CREATE TABLE problems (problem_id integer, contest_id integer, name text, idx text, rating integer, solved_count integer);
+CREATE TABLE problems (problem_id text, contest_id integer, name text, idx text, rating integer, solved_count integer);
 CREATE TABLE tags (id SERIAL, name text);
-CREATE TABLE problem_tags (problem_id integer, tag_id integer);
+CREATE TABLE problem_tags (problem_id text, tag_id integer);
