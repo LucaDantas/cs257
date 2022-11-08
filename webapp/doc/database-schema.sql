d41375541ade3af04f2d527403494346a03d6a38
LucaDantas <luca100sdantas2@gmail.com>
2022-11-08 00:55:07 -0600

    Added difficulty for contests

--- webapp/doc/database-schema.sql
+++ webapp/doc/database-schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (handle text, first_name text, last_name text, country text, organization text, rating integer, max_rating integer, user_rank text, max_user_rank text);
-CREATE TABLE contests (id integer, total_solves integer);
+CREATE TABLE contests (id integer, total_solves integer, difficulty integer);
 CREATE TABLE problems (problem_id text, contest_id integer, name text, rating integer, solved_count integer);
 CREATE TABLE tags (id SERIAL, name text);
 CREATE TABLE problem_tags (problem_id text, tag_id integer);
