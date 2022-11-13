eb4830b50c78b69468760063d6af335f60e71344
LucaDantas <luca100sdantas2@gmail.com>
2022-11-12 23:17:13 -0600

    Created api to query problems, improved users query and created help

--- webapp/api.py
+++ webapp/api.py
@@ -24,6 +24,9 @@ def get_connection():
                             user=config.user,
                             password=config.password)
 
+@api.route('/help')
+def get_help():
+    return '<p>' + '<br>'.join(open('.' + flask.url_for('static', filename='api-design.txt'), 'r').readlines()) + '</p>'
 
 @api.route('/users')
 def get_users():
@@ -71,34 +74,85 @@ def get_users():
 
 
     if len(predicates) > 0:
-        query += " WHERE"
-        index = 0
-        for predicate in predicates:
-            query += " " + predicate
-            if index != len(predicates) - 1:
-                query += " AND"
-            index += 1
+        query += " WHERE " + " AND ".join(predicates)
+
+    query += " ORDER BY (-users.rating, users.handle) LIMIT %(max_users)s"
+
+    args["max_users"] = int(max_users) if max_users else 50
 
     users = []
     try:
         connection = get_connection()
         cursor = connection.cursor()
-        print("query:", query, "args:", args)
         cursor.execute(query, args)
-        max_users = int(max_users) if max_users else 50
-        rows = list(cursor)
-        for row in rows[:min(len(rows), max_users)]:
+
+        for row in list(cursor):
             users.append({"handle": row[0], "first_name": row[1], "last_name": row[2],
                           "rating": row[3], "max_rating": row[4], "user_rank": row[5],
                           "max_user_rank": row[6]})
-        users = sorted(users, key=lambda user: str(user["first_name"]) + str(user["last_name"]))
-        users = sorted(users, key=lambda user: int(user["rating"]), reverse=True)
+
         cursor.close()
         connection.close()
     except Exception as e:
         traceback.print_exc()
 
     return json.dumps(users)
+
+
+@api.route('/problems')
+def get_problems():
+    lowest_rating = flask.request.args.get("lowest_rating")
+    highest_rating = flask.request.args.get("highest_rating")
+    max_problems = flask.request.args.get("max_problems")
+    print("received args:", flask.request.args)
+    query = """SELECT problem_id, name, rating, solved_count FROM problems"""
+
+    predicates = []
+    args = {}
+
+    if lowest_rating:
+        predicates.append("""problems.rating >= %(lowest_rating)s""")
+        args["lowest_rating"] = int(lowest_rating)
+
+    if highest_rating:
+        predicates.append("""problems.rating <= %(highest_rating)s""")
+        args["highest_rating"] = int(highest_rating)
+
+    if len(predicates) > 0:
+        query += " WHERE " + " AND ".join(predicates)
+
+    query += " ORDER BY problems.solved_count DESC LIMIT %(max_problems)s"
+    args["max_problems"] = int(max_problems) if max_problems else 50
+
+    problems = []
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+
+        cursor.execute(query, args)
+        for row in list(cursor):
+            problems.append({"id": row[0], "name": row[1], "rating": row[2], "solved_count": row[3]})
+
+        for problem in problems:
+            local_query= """SELECT tags.name FROM tags, problem_tags
+                            WHERE problem_tags.problem_id = %(id)s
+                            AND problem_tags.tag_id = tags.id"""
+            local_args = {"id" : problem['id']}
+            
+            cursor.execute(local_query, local_args)
+
+            problem["tags"] = []
+            for tag in list(cursor):
+                problem["tags"].append(tag[0])
+            problem["tags"] = ", ".join(problem["tags"])
+
+        cursor.close()
+        connection.close()
+    except Exception as e:
+        traceback.print_exc()
+
+    return json.dumps(problems)
+
 
 @api.route('/tag_names')
 def get_tag_names():
@@ -108,9 +162,10 @@ def get_tag_names():
     try:
         connection = get_connection()
         cursor = connection.cursor()
-        print("query:", query)
-        cursor.execute(query, )
-        tags = list(cursor)
+        cursor.execute(query,)
+        tags = []
+        for tag in list(cursor):
+            tags.append(tag[0])
         cursor.close()
         connection.close()
     except Exception as e:
