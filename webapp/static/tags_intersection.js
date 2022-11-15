ce9c133b21f43861a6d04df79759928f0623a1d7
zimri-leisher <zimri.leisher@gmail.com>
2022-11-15 10:42:55 -0600

    More work on adding tags_intersection

--- webapp/static/tags_intersection.js
+++ webapp/static/tags_intersection.js
@@ -19,25 +19,22 @@ function onSearchButtonClicked() {
     var tagsRequested = [...document.getElementById('tags').options].filter(option => option.selected).map(option => option.value);
     // tagsRequested should be a comma separated list of tags
 
-    var url = getAPIBaseURL() + '/tags_graph/' + tagsRequested;
-    // from api we get a map of tag: [{rating: count at that rating}, ...]
+    var url = getAPIBaseURL() + '/tags_intersection/' + tagsRequested;
+    // from api we get a list [{rating: count at that rating}, ...]
 
     // we want to graph:
     // x axis is rating range
     // y axis is the number of problems with that rating?
-    // graph one line for each tag selected
     
     fetch(url, {method: 'get'})
     .then((response) => response.json())
     .then(function(problemsData) {
         var chartData = [];
-        for(var key in problemsData) {
-            var dataPoints = problemsData[key].map(pt => ({x: pt[0], y: pt[1]}));
-            chartData.push({name: key,
-                            type: "line",
-                            dataPoints: dataPoints,
-                            showInLegend: true});
-        }
+        var dataPoints = problemsData.map(pt => ({x: pt[0], y: pt[1]}));
+        chartData.push({name: tagsRequested,
+                        type: "line",
+                        dataPoints: dataPoints,
+                        showInLegend: true});
         var c = new CanvasJS.Chart("chartContainer", {
             axisX: { title: "Rating of problem" },
             axisY: { title: "Number of problems" },
